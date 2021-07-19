b89adddde1b1bcaed39c02fa5685bd541c46536f
Mateusz Karandys <mateusz.karandys@gmail.com>
2021-07-19 10:14:51 +0200

    card info

--- script.js
+++ script.js
@@ -1,20 +1,22 @@
 const booksContainer = document.querySelector('.books-container');
 const addBookButton = document.querySelector('.new-book');
 
-function Book (id, title, author, pages, isRead) {
+function Book (id, title, author, pages, isRead, rating, insertionDate, readDate) {
     this.id = id;
     this.title = title;
     this.author = author;
     this.pages = pages;
     this.isRead = isRead;
+    this.rating = rating;
+    this.insertionDate = insertionDate;
 }
 
 Book.prototype.info = function() {
     return `${this.id}, ${this.title} by ${this.author}, ${this.pages} pages, ${this.isRead ? 'already read' : 'not read yet'}`;
 }
 
-const theHobbit = new Book(0, 'The Hobbit', 'J.R.R. Tolkien', 295, true);
-const lettersFromGrave = new Book(1, 'Listy Zza Grobu', 'Remigiusz Mróz', 457, false);
+const theHobbit = new Book(0, 'The Hobbit', 'J.R.R. Tolkien', 295, true, 4);
+const lettersFromGrave = new Book(1, 'Listy Zza Grobu', 'Remigiusz Mróz', 457, false, 5);
 
 let library = [theHobbit, lettersFromGrave];
 
@@ -50,7 +52,7 @@ function displayBook(book) {
         <div class="card-content">Author: ${book.author}</div>
         <div class="card-content">Pages: ${book.pages}</div>
         <div class="card-content is-read">Read: ${book.isRead ? 'YES' : 'NO'}</div>
-        <div class="card-content rating">
+        <div class="card-content rating flex-end">
         <span class="material-icons-outlined rating-star">grade</span>
         <span class="material-icons-outlined rating-star">grade</span>
         <span class="material-icons-outlined rating-star">grade</span>
@@ -59,10 +61,16 @@ function displayBook(book) {
         </div>`;
 
     item.innerHTML = itemContent;
+    
+    item.addEventListener('click', () => {
+        const parent = item.parentElement;
+        const itemId = Array.from(parent.childNodes).indexOf(item);
+        const book = library[itemId - 1];
+
+        console.log(itemId);
+        console.log(book);
+        
+    })
 
     booksContainer.appendChild(item);
-
 }
-
-// console.log(Book.prototype);
-console.table(library);
